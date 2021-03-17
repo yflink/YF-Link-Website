c579e0aecea26340f49cc267747bd36f53bf0471
Tim Juretzek <tj@jur-solutions.de>
2021-03-17 16:26:43 +0100

    Updates

---
+++ precache-manifest.fea94432f3a07335b301fba0dc1ae627.js
@@ -0,0 +1,302 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7a06fc38100c3e772e65550bee54c1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f9f1fa6c2dfbd528f6c3",
+    "url": "./static/css/main.fb367ce6.chunk.css"
+  },
+  {
+    "revision": "c22c0b5518b3f6608630",
+    "url": "./static/js/0.bc772e22.chunk.js"
+  },
+  {
+    "revision": "5798f8a9be50420fd32e",
+    "url": "./static/js/10.680f0e60.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "./static/js/10.680f0e60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68206180224bf320b52f",
+    "url": "./static/js/11.d5ca9a09.chunk.js"
+  },
+  {
+    "revision": "890dd8dfaad089959118",
+    "url": "./static/js/12.22ee8a68.chunk.js"
+  },
+  {
+    "revision": "25cb7db5ef4afb165b36",
+    "url": "./static/js/13.3789ebf4.chunk.js"
+  },
+  {
+    "revision": "08c430376b64e25ab83b",
+    "url": "./static/js/14.6165c0ee.chunk.js"
+  },
+  {
+    "revision": "0710c766a460a8c3cacd",
+    "url": "./static/js/3.7e2db5e2.chunk.js"
+  },
+  {
+    "revision": "fa18c0b590889ee8f9b667ae1f0b19a0",
+    "url": "./static/js/3.7e2db5e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9a738cc7aee65953ce8",
+    "url": "./static/js/4.8436d439.chunk.js"
+  },
+  {
+    "revision": "95b85fc57f7f49a2f3a98124773ccfef",
+    "url": "./static/js/4.8436d439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76afee3c4db6c689cbfe",
+    "url": "./static/js/5.f3107ced.chunk.js"
+  },
+  {
+    "revision": "0386d8a9f6b9371c0f0c",
+    "url": "./static/js/6.31f3a11b.chunk.js"
+  },
+  {
+    "revision": "3df54bba2137ec524f3fb39f2c61461a",
+    "url": "./static/js/6.31f3a11b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4618a13da95cb3cb536e",
+    "url": "./static/js/7.efbcd78b.chunk.js"
+  },
+  {
+    "revision": "5943cfb733f7534a1f84",
+    "url": "./static/js/8.a6ffb924.chunk.js"
+  },
+  {
+    "revision": "b2799aa68fbac5d498509a5eb2653b15",
+    "url": "./static/js/8.a6ffb924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db375ee355a4e8bc0040",
+    "url": "./static/js/9.31a2c946.chunk.js"
+  },
+  {
+    "revision": "c178649ae0de162657ef6caa733cc024",
+    "url": "./static/js/9.31a2c946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9f1fa6c2dfbd528f6c3",
+    "url": "./static/js/main.e2c63e95.chunk.js"
+  },
+  {
+    "revision": "7d715694a7e786744b6a",
+    "url": "./static/js/runtime-main.eff66453.js"
+  },
+  {
+    "revision": "f8df0e8a06170b909a2bfa6e8118b418",
+    "url": "./static/media/Formular Regular.f8df0e8a.ttf"
+  },
+  {
+    "revision": "f66b0cf3ab573715ce7fadb693f9702b",
+    "url": "./static/media/LINK.f66b0cf3.png"
+  },
+  {
+    "revision": "b24396917a138794160b24ff04a71d80",
+    "url": "./static/media/WorkSans-VariableFont_wght.b2439691.ttf"
+  },
+  {
+    "revision": "2a965a95888f71b09f268d9715a2f3e1",
+    "url": "./static/media/YFLink-header-logo.2a965a95.svg"
+  },
+  {
+    "revision": "33bbf397b0d4e28ce23f5a1f998d01d9",
+    "url": "./static/media/YFLink-header-options.33bbf397.svg"
+  },
+  {
+    "revision": "29f583ef19c916890c63be8dc28a572c",
+    "url": "./static/media/analytics.29f583ef.svg"
+  },
+  {
+    "revision": "c3ec93b5d5d3da5f51ade736cc3fd471",
+    "url": "./static/media/calculator_1.c3ec93b5.svg"
+  },
+  {
+    "revision": "a84a56ba5f532a65057c6ff75ccc9766",
+    "url": "./static/media/calculator_2.a84a56ba.svg"
+  },
+  {
+    "revision": "5f6dc1df026859346a0dc5d5a665a32e",
+    "url": "./static/media/checked.5f6dc1df.svg"
+  },
+  {
+    "revision": "aa4c7a7647abc7ede02e017c1a0141b6",
+    "url": "./static/media/coinbaseWalletIcon.aa4c7a76.svg"
+  },
+  {
+    "revision": "e3f518748ffcf1ec00cb5c3641bd02f6",
+    "url": "./static/media/dropdown.e3f51874.svg"
+  },
+  {
+    "revision": "1206a7172bb7722200e8107fc784bb9b",
+    "url": "./static/media/gift.1206a717.svg"
+  },
+  {
+    "revision": "d2bbcc712987575dc4f7eb634e075fa8",
+    "url": "./static/media/help.d2bbcc71.svg"
+  },
+  {
+    "revision": "62dc388913b0c0496bd66b3e99fec713",
+    "url": "./static/media/icn-aethereum.62dc3889.svg"
+  },
+  {
+    "revision": "ae009e653a9d5559684442fbac0d5551",
+    "url": "./static/media/icn-ledger.ae009e65.svg"
+  },
+  {
+    "revision": "a357c69f4384506d7970bb4befb88f23",
+    "url": "./static/media/icn-metamask.a357c69f.svg"
+  },
+  {
+    "revision": "31887fe06916f2442ff2d2fa58ebb300",
+    "url": "./static/media/left-candle.31887fe0.svg"
+  },
+  {
+    "revision": "da4137de99d4f5c2d06d033942c00dde",
+    "url": "./static/media/left-cane-small.da4137de.png"
+  },
+  {
+    "revision": "f391757d9b3a57a8ad96936049d70813",
+    "url": "./static/media/left-cane.f391757d.svg"
+  },
+  {
+    "revision": "e1040feb7eb2a767696e1e37d6126883",
+    "url": "./static/media/linkcheck.e1040feb.svg"
+  },
+  {
+    "revision": "702adbd4ad63463c98872bf3d1933649",
+    "url": "./static/media/linklend.702adbd4.svg"
+  },
+  {
+    "revision": "98f680cc61913bbf5ba8b0658b9d54c7",
+    "url": "./static/media/linkpad.98f680cc.svg"
+  },
+  {
+    "revision": "945a3109ca56c1960fcd04a6b5b2636a",
+    "url": "./static/media/linkswap.945a3109.svg"
+  },
+  {
+    "revision": "16dedfcdd818f5e6a72a5f71e7aa9a3c",
+    "url": "./static/media/nft.16dedfcd.svg"
+  },
+  {
+    "revision": "b234b2bfa0417c7e8711c3a8d17afeec",
+    "url": "./static/media/portisIcon.b234b2bf.png"
+  },
+  {
+    "revision": "a04774d7bb9127bc1a60900fdb9872cb",
+    "url": "./static/media/raffle.a04774d7.svg"
+  },
+  {
+    "revision": "532dbd146a6e163ae7dad791c0bf9b9f",
+    "url": "./static/media/right-candle.532dbd14.svg"
+  },
+  {
+    "revision": "b8d62065810bb271835dcea38284cdc9",
+    "url": "./static/media/right-cane.b8d62065.svg"
+  },
+  {
+    "revision": "34298d1b29aebe6749f033ea6f6ccd33",
+    "url": "./static/media/stake.34298d1b.svg"
+  },
+  {
+    "revision": "cc71d74af01695ca80485ee900783d13",
+    "url": "./static/media/swap.cc71d74a.svg"
+  },
+  {
+    "revision": "dc5a08fd9650295c27f5e56e5541e04d",
+    "url": "./static/media/thumbs-down.dc5a08fd.svg"
+  },
+  {
+    "revision": "9a59712042499e58e6855c68e2eb10c9",
+    "url": "./static/media/thumbs-up.9a597120.svg"
+  },
+  {
+    "revision": "887487299adad60d125b7278ef2235b0",
+    "url": "./static/media/ticket.88748729.svg"
+  },
+  {
+    "revision": "edcc1ab5dde5cb3d5cf134c4aade641b",
+    "url": "./static/media/trustWallet.edcc1ab5.png"
+  },
+  {
+    "revision": "2808b5cc288e9b8a9b13b05f04ce474e",
+    "url": "./static/media/vote.2808b5cc.svg"
+  },
+  {
+    "revision": "e9b09404e5536567fe839a42c000e8aa",
+    "url": "./static/media/waffle.e9b09404.svg"
+  },
+  {
+    "revision": "5e81cac236fd057cb686399a8fa2ea57",
+    "url": "./static/media/walletConnectIcon.5e81cac2.svg"
+  },
+  {
+    "revision": "2e91ac5d4755878ce1aaaf8c563a12c9",
+    "url": "./static/media/warning.2e91ac5d.svg"
+  },
+  {
+    "revision": "936b5e8eed6c4502b62fbabb46fe3b2f",
+    "url": "./static/media/yfl-blur-up.936b5e8e.svg"
+  },
+  {
+    "revision": "fd6b51b795d0d4b7e79addda5f5452f2",
+    "url": "./static/media/yfl-coingecko-logo.fd6b51b7.svg"
+  },
+  {
+    "revision": "b893437d325c722e7b7bafba004170a0",
+    "url": "./static/media/yfl-coinmarketcap-logo.b893437d.svg"
+  },
+  {
+    "revision": "4b5265e7e915c202d7e8d830430b9c6b",
+    "url": "./static/media/yfl-debank-logo.4b5265e7.svg"
+  },
+  {
+    "revision": "72166bcaafd57271f39df889b7b8a1ab",
+    "url": "./static/media/yfl-defipulse-logo.72166bca.svg"
+  },
+  {
+    "revision": "62e23754c550bcfb9eeff371601b04f3",
+    "url": "./static/media/yfl-discord-logo.62e23754.svg"
+  },
+  {
+    "revision": "3d0c62986dd88403c69e0b125fb6c613",
+    "url": "./static/media/yfl-down.3d0c6298.svg"
+  },
+  {
+    "revision": "9a28e971921524c8da797438e7511a05",
+    "url": "./static/media/yfl-github-logo.9a28e971.svg"
+  },
+  {
+    "revision": "162098f99fea44471dc64a2bd74111cf",
+    "url": "./static/media/yfl-medium-logo.162098f9.svg"
+  },
+  {
+    "revision": "2082d7f734f008413755ae8ec3885cf2",
+    "url": "./static/media/yfl-telegram-logo.2082d7f7.svg"
+  },
+  {
+    "revision": "cfcb4f8b8609451499584aaa98ad8c3b",
+    "url": "./static/media/yfl-twitter-logo.cfcb4f8b.svg"
+  },
+  {
+    "revision": "567dc1eddfec7cb0b77bd22d96ab95a8",
+    "url": "./static/media/yfl-up.567dc1ed.svg"
+  },
+  {
+    "revision": "b0b9893cd5c05cecf3c37463ca330d4f",
+    "url": "./static/media/yfl-zerion-logo.b0b9893c.svg"
+  },
+  {
+    "revision": "229bb205381fe63aa545998d0a23f0d2",
+    "url": "./static/media/yflusd-logo.229bb205.svg"
+  }
+]);
